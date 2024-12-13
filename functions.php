8f6679f0afae103610663486191782703e8f7e75
Burhan Aftab <burhanaftabali@gmail.com>
2024-12-13 16:11:32 +0500

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,256 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Setup Theme
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+//* Subpage Header Code
+//require_once('subpage-header.php');
+
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'parallax', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'parallax' ) );
+
+//* Add Image upload to WordPress Theme Customizer
+add_action( 'customize_register', 'parallax_customizer' );
+function parallax_customizer(){
+	require_once( get_stylesheet_directory() . '/lib/customize.php' );
+}
+
+//* Include Section Image CSS
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+global $blogurl;
+$blogurl = get_stylesheet_directory_uri();
+
+//* Enqueue scripts and styles
+add_action( 'wp_enqueue_scripts', 'parallax_enqueue_scripts_styles' );
+function parallax_enqueue_scripts_styles() {
+	// Styles
+	wp_enqueue_style( 'dashicons' );
+	wp_enqueue_style( 'custom', get_stylesheet_directory_uri() . '/css/allstyles.css', array() );
+	wp_enqueue_style( 'PTSans', get_stylesheet_directory_uri() . '/fonts/PTSans/PTSans.css', array() );
+	
+	//wp_enqueue_style( 'googlefonts', '//fonts.googleapis.com/css?family=PT+Sans:400,700', array() );	
+	//wp_enqueue_style( 'custom-font-awesome', get_stylesheet_directory_uri() . '/css/customfontawesome.min.css', array() );
+	//wp_enqueue_style( 'Fonts', get_stylesheet_directory_uri() . '/fonts/fonts.css', array() );
+
+	// Scripts
+	wp_enqueue_script( 'responsive-menu-js', get_stylesheet_directory_uri() . '/js/responsive-menu/responsive-menu.js', array( 'jquery' ), '1.0.0' );
+
+	// Examples & Optionals:
+	//wp_enqueue_script( 'owlcarousel', get_stylesheet_directory_uri() . '/js/owl-carousel/owl.carousel.min.js', array('jquery') );
+	//wp_enqueue_style( 'ptsans', 'https://fonts.googleapis.com/css?family=PT+Sans:400,700', array() );
+	//wp_enqueue_style( 'ptsans-caption', 'https://fonts.googleapis.com/css?family=PT+Sans+Caption:400,700', array() );
+	//wp_enqueue_style( 'owlcarouselcss', get_stylesheet_directory_uri() . '/js/owl-carousel/owl.carousel.css', array() );
+	//wp_enqueue_style( 'fancyBoxcss', get_stylesheet_directory_uri() . '/js/fancy/jquery.fancybox.css', array() );
+	
+}
+
+// Removes Query Strings from scripts and styles
+function remove_script_version( $src ){
+  if ( strpos( $src, 'uploads/bb-plugin' ) !== false || strpos( $src, 'uploads/bb-theme' ) !== false ) {
+    return $src;
+  }
+  else {
+    $parts = explode( '?ver', $src );
+    return $parts[0];
+  }
+}
+add_filter( 'script_loader_src', 'remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', 'remove_script_version', 15, 1 );
+
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Reposition the primary navigation menu
+//remove_action( 'genesis_after_header', 'genesis_do_nav' );
+//add_action( 'genesis_header', 'genesis_do_nav', 12 );
+
+// Add Search to Primary Nav
+//add_filter( 'genesis_header', 'genesis_search_primary_nav_menu', 10 );
+function genesis_search_primary_nav_menu( $menu ){
+    locate_template( array( 'searchform-header.php' ), true );
+}
+
+//* Add support for structural wraps
+add_theme_support( 'genesis-structural-wraps', array(
+	'header',
+	'nav',
+	'subnav',
+	'breadcrumb',
+	'footer-widgets',
+	'footer',
+) );
+
+// Add Read More Link to Excerpts
+add_filter('excerpt_more', 'get_read_more_link');
+add_filter( 'the_content_more_link', 'get_read_more_link' );
+function get_read_more_link() {
+   return '...&nbsp;<a class="readmore" href="' . get_permalink() . '">Read&nbsp;More &raquo;</a>';
+}
+
+//* Add support for 4-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 4 );
+
+//* Customize the entry meta in the entry header (requires HTML5 theme support)
+add_filter( 'genesis_post_info', 'sp_post_info_filter' );
+function sp_post_info_filter($post_info) {
+	$post_info = '[post_date] [post_comments] [post_edit]';
+	return $post_info;
+}
+
+//* Custom Breadcrumb Hook 
+function breadcrumb_hook() {
+	do_action('breadcrumb_hook');
+}
+
+//* Remove breadcrumbs and reposition them
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+add_action( 'breadcrumb_hook', 'genesis_do_breadcrumbs', 12 );
+
+// Modify Breadcrumbs Args
+add_filter( 'genesis_breadcrumb_args', 'malcolm_breadcrumb_args' );
+function malcolm_breadcrumb_args( $args ) {
+	$args['prefix'] = '<div class="breadcrumbs"><div class="wrap">';
+	$args['suffix'] = '</div></div>';
+	$args['sep'] = ' <span class="bread-sep">></span> ';
+	$args['heirarchial_attachments'] = true;
+	$args['heirarchial_categories'] = true;
+	$args['display'] = true;
+	$args['labels']['prefix'] = '';
+    return $args;
+}
+
+// Widget - Latest News on home page
+genesis_register_sidebar( array(
+	'id'			=> 'home-latest-news',
+	'name'			=> __( 'Latest News on Home Page', 'thrive' ),
+	'description'	=> __( 'This is latest news home page widget', 'thrive' ),
+) );
+
+// Blog Widgets
+genesis_register_sidebar( array(
+	'id'			=> 'blog-sidebar',
+	'name'			=> __( 'Blog Widgets', 'thrive' ),
+	'description'	=> __( 'This is latest news widget', 'thrive' ),
+) );
+
+// Add Header Links Widget to Header
+//add_action( 'genesis_before', 'header_widget', 1 );
+	function header_widget() {
+	if (is_active_sidebar( 'header-links' ) ) {
+ 	genesis_widget_area( 'header-links', array(
+		'before' => '<div class="header-links">',
+		'after'  => '</div>',
+	) );
+}}
+
+// Unregister unused sidebar
+//unregister_sidebar( 'header-right' );
+
+// Previous / Next Post Navigation Filter For Genesis Pagination
+add_filter( 'genesis_prev_link_text', 'gt_review_prev_link_text' );
+function gt_review_prev_link_text() {
+        $prevlink = '&laquo;';
+        return $prevlink;
+}
+add_filter( 'genesis_next_link_text', 'gt_review_next_link_text' );
+function gt_review_next_link_text() {
+        $nextlink = '&raquo;';
+        return $nextlink;
+}
+
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_after_header', 'genesis_do_nav', 12 );
+
+// Add Additional Image Sizes
+add_image_size( 'genesis-post-thumbnail', 163, 108, true );
+add_image_size( 'subpage-header', 1600, 162, true );
+add_image_size( 'news-thumb', 260, 150, false );
+add_image_size( 'news-full', 800, 300, false );
+add_image_size( 'sidebar-thumb', 200, 150, false );
+
+
+// Gravity Forms confirmation anchor on all forms
+add_filter( 'gform_confirmation_anchor', '__return_true' );
+
+
+// Button Shortcode
+// Usage: [button url="https://www.google.com"] Button Shortcode [/button]
+function button_shortcode($atts, $content = null) {
+  extract( shortcode_atts( array(
+	  'url' => '#',
+	  'target' => '_self',
+	  'onclick' => '',
+
+  ), $atts ) 
+);
+return '<a target="' . $target . '" href="' . $url . '" class="button" onClick="' . $onclick . '"><span>' . do_shortcode($content) . '</span></a>';
+}
+add_shortcode('button', 'button_shortcode');
+
+// Link Shortcode
+// Usage: [link url=”tel:1-817-447-9194″ onClick=”onClick=”ga(‘send’, ‘event’, { eventCategory: ‘Click to Call’, eventAction: ‘Clicked Phone Number’, eventLabel: ‘Header Number’});”]
+function link_shortcode($atts, $content = null) {
+  extract( shortcode_atts( array(
+	  'url' => '#',
+	  'target' => '_self',
+	  'onclick' => '',
+  ), $atts ) 
+);
+return '<a target="' . $target . '" href="' . $url . '" onClick="' . $onclick . '">' . do_shortcode($content) . '</a>';
+}
+add_shortcode('link', 'link_shortcode');
+
+// Run shortcodes in Text Widgets
+add_filter('widget_text', 'do_shortcode');
+
+
+//Removing unused Default Wordpress Emoji Script - Performance Enhancer
+function disable_emoji_dequeue_script() {
+    wp_dequeue_script( 'emoji' );
+}
+add_action( 'wp_print_scripts', 'disable_emoji_dequeue_script', 100 );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); 
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+// Removes Emoji Scripts 
+add_action('init', 'remheadlink');
+function remheadlink() {
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'index_rel_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'feed_links', 2);
+	remove_action('wp_head', 'feed_links_extra', 3);
+	remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+	remove_action('wp_head', 'start_post_rel_link', 10, 0);
+	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+	remove_action('wp_head', 'wp_shortlink_header', 10, 0);
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+}
+
+// Add "nav-primary" class to Main Menu as this gets removed when we reposition the menu inside header/widget area
+add_filter( 'genesis_attr_nav-header', 'thrive_custom_nav_id' );
+function thrive_custom_nav_id( $attributes ) {
+ 	$attributes['class'] = 'nav-primary';
+ 	return $attributes;
+}
+
+//Sets the number of revisions for all post types
+add_filter( 'wp_revisions_to_keep', 'revisions_count', 10, 2 );
+function revisions_count( $num, $post ) {
+	$num = 3;
+    return $num;
+}
+
+ // Remove Footer
+remove_action('genesis_footer', 'genesis_do_footer');
+remove_action('genesis_footer', 'genesis_footer_markup_open', 5);
+remove_action('genesis_footer', 'genesis_footer_markup_close', 15);
